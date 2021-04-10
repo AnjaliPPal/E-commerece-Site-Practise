194a9bbfe4bd27a107f9bf08e6be08022dfab5ed
anjalipal2001 <ap7197339@gmail.com>
2021-04-10 15:48:54 +0530

    Updates

---
+++ precache-manifest.4c100e0a58b4f2bbb2568de1cbfcec56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d5f9eaba1be4a8bb104565f5a3091b",
+    "url": "/qwe/index.html"
+  },
+  {
+    "revision": "e3add597fea01326a05a",
+    "url": "/qwe/static/css/main.53afffa0.chunk.css"
+  },
+  {
+    "revision": "597789830f7c2ac63c96",
+    "url": "/qwe/static/js/2.85ad0001.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/qwe/static/js/2.85ad0001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3add597fea01326a05a",
+    "url": "/qwe/static/js/main.57a02f9b.chunk.js"
+  },
+  {
+    "revision": "f1b693a2b87b2c75025d",
+    "url": "/qwe/static/js/runtime-main.d26cae91.js"
+  }
+]);
